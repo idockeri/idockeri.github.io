cd42770f403c9566ff0209f73fd608a77da77a98
idockeri <69664336+idockeri@users.noreply.github.com>
2023-05-03 18:06:55 +0800

    Update

--- index.php
+++ index.php
@@ -1,35 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Главная страница</title>
-    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-    <div class="top"></div>
-    <header class="header">
-        <img src="img/logo.png" alt="Logo">
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Galery</a></li>
-            <li><a href="#">About</a></li>
-            <li><button>Sign In</button></li>
-            <li><button>Sign Up</button></li>
-        </ul>
-    </header>
-    <main>
-        <div id="page1">
-            <h1>Атст</h1>
-            <p>Текст</p>
-        </div>
-        <div id="page2"></div>
-        <div id="page3"></div>
-    </main>
-    <footer>
-        <p>&copy 2023 Logo</p>
-    </footer>   
+    <h1>Ку</h1>
 </body>
 </html>
